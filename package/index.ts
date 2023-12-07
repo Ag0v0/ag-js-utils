2656aa9b2f27730f045edbb9dce9c21f10edbe0a
Ag <ag@ciglink.com>
2023-12-07 17:52:45 +0800

    0.1.0

---
+++ package/index.ts
@@ -0,0 +1,4 @@
+export * from "./lib/common";
+export * as mimeType from "./lib/mime";
+export * as Queue from "./lib/Queue";
+export * as accMath from "./lib/accMath";
--- src/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./common";
-export * as mimeType from "./mime";
-export * as Queue from "./Queue";
-export * as accMath from "./accMath";
